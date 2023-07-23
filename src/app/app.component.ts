12c037fde2580bfa4db7516c9215b19b8c8be4d1
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-07-23 17:46:36 +0200

    started header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['apple.jpg', 'beach.jpg', 'source.jpg'];
-  currentImage = 0;
-  showImage = true;
+export class AppComponent{
 
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(()=>{
-        this.showImage = true;
-      }, 10)
-    }, 5000);
-  }
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit{
+  images = ['apple.jpg', 'beach.jpg', 'source.jpg'];
+  headline = [
+    'Learn Software Engineering',
+    'Work from anywhere',
+    'Everything in your hands',
+  ];
+
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 5000);
+  }
+}
