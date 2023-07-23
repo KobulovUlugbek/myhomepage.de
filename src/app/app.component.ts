ac07eb10ab4e4ca8105ead3b316bf67b119463bf
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-07-23 15:34:37 +0200

    added images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'MyHomepage';
+export class AppComponent implements OnInit {
+  images = ['apple.jpg', 'beach.jpg', 'source.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(()=>{
+        this.showImage = true;
+      }, 10)
+    }, 5000);
+  }
 }
